fa017a52e53cf9fcadddfa8b919cdaabb6ab4d07
Spedoske <leomundspedoske@gmail.com>
2021-10-22 15:31:52 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+        <div className="flex-shrink-0">
+        </div>
+        <div>
+          <div className="text-xl font-medium text-black">ChitChat</div>
+          <p className=" text-gray-500">You have a new message!</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
