3a14235a8960e5aa5948ce28a52055c5ac6c4758
Spedoske <leomundspedoske@gmail.com>
2021-10-28 21:25:30 +0800

    initial commit

---
+++ src/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Footer(){
+    return <section className="bg-indigo-900 text-white">
+        I am footer
+    </section>
+}
+
+export default Footer;
---
+++ src/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Navbar(){
+    return <div className="bg-gray-300 h-16">
+        NavBar
+    </div>
+}
+
+export default Navbar;
