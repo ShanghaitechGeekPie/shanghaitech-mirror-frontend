5cc2ce4da91e35d147ad6ffe5338ac6f1ee90de1
Spedoske <leomundspedoske@gmail.com>
2021-10-28 21:25:30 +0800

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,27 @@
 import React from 'react';
-import CallApp from 'callapp-lib';
-
-const option = {
-    scheme: {
-        protocol: 'wand_scheme',
-    },
-    intent: {
-        package: 'com.zhihu.android',
-        scheme: 'wand_scheme',
-    },
-    universal: {
-        host: 'auth.member.dgene.com',
-    },
-    appstore: 'https://itunes.apple.com/app/id1574341319',
-    fallback: 'https://anome.geekpie.club/',
-    timeout: 2000,
-};
-
-const lib = new CallApp(option);
-
-function RedirectToApps(){
-    const search = new URLSearchParams(window.location.search);
-    const config = {path: `?${search.toString()}`};
-    lib.open(config);
-}
-
-function CallTest(){
-    const search = new URLSearchParams(window.location.search);
-    const config = {path: `?${search.toString()}`};
-    let generateScheme = lib.generateScheme(config);
-    let generateIntent = lib.generateIntent(config);
-    let generateUniversalLink= lib.generateUniversalLink(config);
-    console.log("generateScheme: ",lib.generateScheme(config))
-    console.log("generateIntent: ",lib.generateIntent(config))
-    console.log("generateUniversalLink: ",lib.generateUniversalLink(config))
-    return (<div>
-        <div className="">
-            <span className="block w-full">测试: http://localhost:3000/?user_id=1&style_id=2</span>
-            <span className="block w-full">generateScheme:{generateScheme}</span>
-            <span className="block w-full">generateIntent:{generateIntent}</span>
-            <span className="block w-full">generateUniversalLink:{generateUniversalLink}</span>
-        </div>
-    </div>)
-}
+import MirrorStatus from "./MirrorStatus";
+import Navbar from "./Navbar";
+import News from "./News";
+import Banner from "./Banner";
+import Footer from "./Footer";
 
 function App() {
     return (
         <div className="App">
-            <div className="w-screen h-screen bg-white">
-                <p>暂未实现安卓端跳转，一是因为手机上没装，不好调试。二是安卓端跳转需要得到包名(dgene.com).<br/>
-                </p>
-                <CallTest/>
-                <div className="flex items-center justify-center h-24">
-                    <span className="block text-center rounded-full px-4 py-2 bg-blue-500 font-semibold text-gray-100 tracking-wider shadow-lg" onClick={RedirectToApps}>App 内打开</span>
+            <div className="w-full bg-gray-100">
+                <div className="w-11/12 lg:w-5/6 mx-auto">
+                    <Navbar/>
+                    <div className="flex flex-col w-full">
+                        <Banner/>
+                        <span className="text-xl font-semibold mb-2 text-transparent bg-clip-text bg-gradient-to-r from-pink-400 to-red-600">镜像列表</span>
+                        <div className="flex flex-col lg:flex-row w-full">
+                            <MirrorStatus/>
+                            <News/>
+                        </div>
+                    </div>
                 </div>
             </div>
+            <Footer/>
         </div>
     );
 }
---
+++ src/Banner.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Banner(){
+    return <section className="w-full mb-12">
+        <h1 className="font-bold text-4xl tracking-widest text-transparent bg-clip-text bg-gradient-to-r from-pink-400 to-red-600">抽风、龟速、复古的镜像服务。</h1>
+        <span className="block text-gray-700 mt-2 text-lg tracking-wide">托管于华西教育网骨干节点张江理工学院。</span>
+    </section>
+}
+
+export default Banner;
