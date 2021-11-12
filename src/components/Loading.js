d89a5903229a21d21c801e414610ba7895b0fd9b
wenxuanjun <wendster@163.com>
2021-11-12 17:02:39 +0800

    Fix: Absolute path of js file loaded in html

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,5 +1,6 @@
 import Typography from '@mui/material/Typography'
 import Container from '@mui/material/Container'
+import Box from '@mui/material/Box'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import CircularProgress from '@mui/material/CircularProgress'
@@ -8,8 +9,10 @@ export default () => (
   <Container maxWidth="lg" sx={{ pt: 8, pb: 6 }}>
     <Card>
       <CardContent>
-        <CircularProgress />
-        <Typography component="div" variant="h5" gutterBottom>Loading</Typography>
+        <Box sx={{ mx: 'auto', textAlign: 'center', pt: 4, pb: 4 }}>
+          <CircularProgress />
+          <Typography component="div" variant="h5" gutterBottom>Loading</Typography>
+        </Box>
       </CardContent>
     </Card>
   </Container>
