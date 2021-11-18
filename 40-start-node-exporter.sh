82e5554c15222695a7cb92cdd381c212117ff20f
gesrua <gesrua@gmail.com>
2021-11-18 17:22:44 +0800

    更新构建

--- 40-start-node-exporter.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-set -e
-
-node_exporter --collector.disable-defaults --collector.filesystem --collector.cpu --collector.cpufreq --collector.diskstats --collector.meminfo --collector.netdev --collector.netclass &
