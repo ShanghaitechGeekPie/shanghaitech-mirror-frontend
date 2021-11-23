2160b4291153f206600daac8953d8f2e7f19fef7
wenxuanjun <wendster@163.com>
2021-11-23 20:55:31 +0800

    Fix: Wrong padding

--- src/components/global/Failed.js
+++ src/components/global/Failed.js
@@ -1,17 +1,14 @@
 import Typography from '@mui/material/Typography'
-import Container from '@mui/material/Container'
 import Box from '@mui/material/Box'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 
 export default () => (
-  <Container maxWidth="lg">
-    <Card>
-      <CardContent>
-        <Box sx={{ mx: 'auto', textAlign: 'center', pt: 4, pb: 4 }}>
-          <Typography variant="h5" sx={{ fontWeight: 'medium' }} gutterBottom>Failed</Typography>
-        </Box>
-      </CardContent>
-    </Card>
-  </Container>
+  <Card>
+    <CardContent>
+      <Box sx={{ mx: 'auto', textAlign: 'center', pt: 4, pb: 4 }}>
+        <Typography variant="h5" sx={{ fontWeight: 'medium' }} gutterBottom>Failed</Typography>
+      </Box>
+    </CardContent>
+  </Card>
 )
--- src/views/Explorer.js
+++ src/views/Explorer.js
@@ -1,5 +1,6 @@
 import { useQuery } from 'react-query'
 import { useLocation } from 'react-router-dom'
+import Container from '@mui/material/Container'
 import List from "@/components/views/List"
 import Loading from "@/components/global/Loading"
 import Failed from "@/components/global/Failed"
@@ -7,10 +8,18 @@ import Config from 'Config'
 
 export default () => {
   const location = useLocation()
-  const { isLoading, isError, data } = useQuery(['summarydata', {path: location.pathname}], () =>
+  const { isLoading, isError, data } = useQuery(['summarydata', { path: location.pathname }], () =>
     fetch(Config.serverUrl + '/api/v1' + location.pathname).then(result => result.json())
   )
-  if (isLoading) return <Loading />
-  if (isError) return <Failed />
+  if (isLoading) return (
+    <Container maxWidth="lg">
+      <Loading />
+    </Container>
+  )
+  if (isError) return (
+    <Container maxWidth="lg">
+      <Failed />
+    </Container>
+  )
   return <List data={data} />
 }
