ca7e79960a249ea0a1cdcfe2461484cf0922f9d2
wenxuanjun <wendster@163.com>
2021-11-23 20:55:31 +0800

    Fix: Wrong padding

--- src/components/global/Loading.js
+++ src/components/global/Loading.js
@@ -1,19 +1,16 @@
 import Typography from '@mui/material/Typography'
-import Container from '@mui/material/Container'
 import Box from '@mui/material/Box'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import CircularProgress from '@mui/material/CircularProgress'
 
 export default () => (
-  <Container maxWidth="lg">
-    <Card>
-      <CardContent>
-        <Box sx={{ mx: 'auto', textAlign: 'center', pt: 4, pb: 4 }}>
-          <CircularProgress />
-          <Typography variant="h5" sx={{ fontWeight: 'medium' }} gutterBottom>Loading</Typography>
-        </Box>
-      </CardContent>
-    </Card>
-  </Container>
+  <Card>
+    <CardContent>
+      <Box sx={{ mx: 'auto', textAlign: 'center', pt: 4, pb: 4 }}>
+        <CircularProgress />
+        <Typography variant="h5" sx={{ fontWeight: 'medium' }} gutterBottom>Loading</Typography>
+      </Box>
+    </CardContent>
+  </Card>
 )
--- src/views/Home.js
+++ src/views/Home.js
@@ -7,7 +7,7 @@ import News from '@/components/views/News'
 export default () => (
   <Container maxWidth="lg">
     <Typography variant="h4" sx={{ fontWeight: 'bold' }} gutterBottom>抽风、龟速、复古的镜像服务</Typography>
-    <Typography variant="h6" gutterBottom sx={{ mb: 8 }}>托管于华西教育网骨干节点张江理工学院</Typography>
+    <Typography variant="h6" gutterBottom sx={{ mb: 6 }}>托管于华西教育网骨干节点张江理工学院</Typography>
     <Grid container alignItems="flex" spacing={4}>
       <Grid item xs={12} md={8}>
         <Mirrors />
