c894e50ef4b7a7e0103bd70ab5674e5ca6b0838f
wenxuanjun <wendster@163.com>
2021-11-25 00:14:49 +0800

    New: Transition

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { Fragment } from 'react'
 import GlobalStyles from '@mui/material/GlobalStyles'
 import CssBaseline from '@mui/material/CssBaseline'
+import Box from '@mui/material/Box'
 import Header from '@/components/global/Header'
 import Router from '@/router/index'
+import { useLocation } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ThemeProvider, createTheme, responsiveFontSizes } from '@mui/material/styles'
 import useMediaQuery from '@mui/material/useMediaQuery'
 import getTheme from '@/styles/theme'
-import '@/styles/transition.css'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -20,18 +20,19 @@ const queryClient = new QueryClient({
 })
 
 export default () => {
+  const location = useLocation()
   const darkmode = useMediaQuery('(prefers-color-scheme: dark)')
   const theme = responsiveFontSizes(createTheme(getTheme(darkmode)))
   return (
-    <Fragment>
-      <QueryClientProvider client={queryClient}>
-        <ThemeProvider theme={theme}>
-          <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
-          <CssBaseline />
-          <Header />
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
+        <CssBaseline />
+        <Header />
+        <Box sx={{ pb: 8 }}>
           <Router />
-        </ThemeProvider>
-      </QueryClientProvider>
-    </Fragment>
+        </Box>
+      </ThemeProvider>
+    </QueryClientProvider>
   )
 }
