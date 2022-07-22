f30aaeaba29f25465e71ad6f4da6a608b77e6d0e
wenxuanjun <wendster@163.com>
2022-07-22 22:18:06 +0800

    Fix: Almost full PWA support test

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from 'react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { unstable_ClassNameGenerator } from '@mui/material/className'
 import { styled, useTheme, ThemeProvider, createTheme, responsiveFontSizes } from '@mui/material/styles'
 import { Scrollbars } from 'react-custom-scrollbars'
