e40c96cf84944dae09d4837e73619981f66fb9b1
wenxuanjun <wendster@163.com>
2022-08-24 14:05:35 +0800

    New: Git http backend support

--- start.sh
+++ start.sh
@@ -3,4 +3,5 @@
 set -e
 
 node_exporter --collector.disable-defaults --collector.filesystem --collector.cpu --collector.cpufreq --collector.diskstats --collector.meminfo --collector.netdev --collector.netclass &
+spawn-fcgi -s /var/run/fcgiwrap.sock /usr/bin/fcgiwrap
 nginx -g "daemon off;"
